d295fff37186440c2097aec4cc3b60c5d4c5874d
mollymillk <kristey.z39@gmail.com>
2022-09-16 15:55:09 +0300

    header + global colors + fonts

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import './Header.sass';
 
-export const Header = (): JSX.Element => {
-	return <div>Header</div>;
+export const Header = ():JSX.Element => {
+	return <header className='header'>
+		<div className='logo'>food store</div>
+		<nav className='main_nav'>
+			<div className='menu_item'>Акции</div>
+			<div className='menu_item'>Продукты</div>
+			<div className='menu_item'>Блог</div>
+			<div className='menu_item'>Контакты</div>
+		</nav>
+		<nav className='icon_nav'>
+			<div className='menu_item'>Поиск</div>
+			<div className='menu_item'>Аккаунт</div>
+			<div className='menu_item'>Корзина</div>
+		</nav>
+	</header>;
 };
